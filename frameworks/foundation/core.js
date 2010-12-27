12816f823e1d10b6a1debb498d029d1a4f5cfc44
Michael Cohen <michael.lee.cohen@gmail.com>
2010-12-26 22:54:01 -0500

    added core.js file to foundation sub-framework

---
+++ frameworks/foundation/core.js
@@ -0,0 +1,7 @@
+// ==========================================================================
+// Project:   Mata - Additional Stuff for SproutCore
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+window.Mata = window.Mata || SC.Object.create();
+window.MATA = window.MATA || window.Mata ;
